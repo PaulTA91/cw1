5e46dcab361c1ef899a02751df6fa523f5c00ff6
PaulTA91 <parmst202@caledonian.ac.uk>
2021-11-05 10:47:47 +0000

    complete code change, removed validateInput and added validation to main function

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,56 +1,35 @@
-import java.util.Scanner;
+class Dec2Hex {
 
-class Dec2Hex
-{
+	public static int Arg1;
 
-    public static int Arg1;
+	public static void main(String args[]) {
+		if (args.length == 0) {
+			System.out.print("No input detected. Try Again.");
+			return;
+		}
+		if (Arg1 != Integer.parseInt(args[0])) {
+			System.out.println("Value must be a positive Int");
+			return;
+		} else if (Arg1 <= 0) {
+			System.out.println("Value must be a positive Int");
+			return;
+		}
 
-    public static void main(String args[])
-    {
-        Arg1 = validateInput();
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+		char ch[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		int rem, num;
+		num = Arg1;
+		String hexadecimal = "";
+		System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
+		while (num != 0) {
+			rem = num % 16;
+			hexadecimal = ch[rem] + hexadecimal;
+			num = num / 16;
+		}
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-	System.out.println("Ending Program");
+		System.out.println("Hexadecimal representation is: " + hexadecimal);
+		System.out.println("Ending Program");
 
-    }
-
-		public static int validateInput() {
-		int input = 0;
-		boolean valid;
-		Scanner sc = new Scanner(System.in);
-
-		do {
-			valid = true;
-			System.out.println("Please enter a positive integer to convert to hexadecimal: ");
-
-			if (sc.hasNextInt()) {
-				input = sc.nextInt();
-				sc.nextLine();
-
-				if (input <= 0) {
-					System.out.println("Input not recognized. Try again.");
-					valid = false;
-				}
-
-			} else {
-				System.out.println("Input not recognized. Try again.");
-				valid = false;
-				sc.next();
-			}
-		} while (!valid);
-		return input;
 	}
-}
 
+}
